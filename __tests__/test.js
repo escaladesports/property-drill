91ef1075df3c4b4b8df613d4e974d7d428244f90
Kennedy Rose <kennedy@kennedyrose.com>
2018-05-25 09:02:56 -0500

    Tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,8 +1,36 @@
-import theModule from '../dist'
+import propDrill from '../dist'
 
-describe('Default module', () => {
-	it('Should have content', () => {
-		const testVar = theModule()
-		expect(testVar).toBeDefined()
+const obj = {
+	a: {
+		b: true,
+		c: false,
+		d: 'asdf',
+	},
+	d: 5,
+	e: {
+		f: [1, 2, 3],
+	},
+}
+
+describe(`Property drill`, () => {
+	it(`Should find object property`, () => {
+		expect(
+			propDrill(obj, `a`, `b`)
+		).toEqual(true)
+	})
+	it(`Should find array value`, () => {
+		expect(
+			propDrill(obj, `e`, `f`, 1)
+		).toEqual(2)
+	})
+	it(`Should be undefined if no args`, () => {
+		expect(
+			propDrill(obj)
+		).toEqual(undefined)
+	})
+	it(`Should be undefined if too deep`, () => {
+		expect(
+			propDrill(obj, `a`, `c`, `z`, 5)
+		).toEqual(undefined)
 	})
 })
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-function TestModule() {
-	const def = {
-		test: '123'
+function propDrill() {
+	let obj = arguments[0]
+	let l = arguments.length
+	let last = l - 1
+	let i
+	let part
+	for (i = 1; i < l; i++) {
+		part = arguments[i]
+		if (typeof obj[part] === 'object') {
+			obj = obj[part]
+			continue
+		}
+		if (i === last) {
+			return obj[part]
+		}
+		return
 	}
-	const obj = {
-		anotherTest: 'abc',
-		...def,
-	}
-	return obj
 }
 
-export default TestModule
+export default propDrill
